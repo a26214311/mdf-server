ecf25e02166b8b5c23eca2705997925554544a16
shikong <a26214311@gmail.com>
2017-05-23 16:11:01 +0800

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+var util = require('util');
+var URL = require('url');
+app.use(express.static('./static'));
+var roomtable = {};
+app.ws('/join/*', function(ws, req) {
+  var path = req.path.substring(1);
+  var pa = path.split("/");
+  var roomid = pa[1];
+  var room = roomtable[roomid];
+  if(room==undefined){
+    room = [ws];
+    roomtable[roomid]= room;
+  }else{
+    if(room.length==1){
+      room.push(ws);
+      roomtable[roomid] = room;
+    }else{
+      console.log("room is full");
+      var ret = {r:128};
+      ws.send(JSON.stringify(ret));
+      ws.close();
+    }
+  }
+  util.inspect(ws);
+  ws.on('message', function(msg) {
+    var room = roomtable[roomid];
+    if(room&&room.length==2){
+      var ws1 = room[0];
+      var ws2 = room[1];
+      ws1.send(msg);
+      ws2.send(msg);
+    }else{
+      var ret = {r:102};
+      ws.send(JSON.stringify(ret));
+    }
+  });
+  ws.on('close', function(msg) {
+    var room = roomtable[roomid];
+    delete(roomtable[roomid]);
+    if(room&&room.length==2){
+      var ws1 = room[0];
+      var ws2 = room[1];
+      ws1.close();
+      ws2.close();
+    }
+  });
+});
+app.listen(3000);  
